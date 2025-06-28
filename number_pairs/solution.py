d95b29772c5baa224a6ed97bf1df2673acf6d2fa
Lukas Dullinger <itislu.git@gmail.com>
2025-06-28 18:41:18 +0200

    feat(number_pairs): Add fast solution solving all N

--- number_pairs/solution.py
+++ number_pairs/solution.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from typing import List, Tuple
+from typing import List
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils import runner
@@ -8,28 +8,19 @@ from utils import runner
 
 def solve(input: List[str]) -> List[str]:
     result = []
+
     for line in input[1:]:
         if len(line) == 0:
             continue
+
         distance = int(line)
-        if calc_digit_sum(distance) % 9 != 0:
-            result.append("None")
+        if distance % 9 != 0:
+            result.append("NONE")
         else:
-            result.append(" ".join(str(pair) for pair in find_digit_sum_pair(distance)))
+            n = distance // 9
+            result.append(f"{n} {n * 10}")
 
     return result
-
-
-def calc_digit_sum(num: int) -> int:
-    return sum(int(c) for c in str(num))
-
-
-def find_digit_sum_pair(distance: int) -> Tuple[int, int]:
-    num = 1
-    while True:
-        if calc_digit_sum(num) == calc_digit_sum(num + distance):
-            return (num, num + distance)
-        num += 1
 
 
 if __name__ == "__main__":
